e7a2c4eef92d92e4a2ddf34da998c5030f86e8fa
yuki <ichijikumanjyu@gmail.com>
2025-10-08 12:43:45 +0900

    出品ページ

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,12 +1,28 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: [:new]
 
   def index
   end
 
   def new
     @user = User.new
+    @item = Item.new
   end
 
-  
+  private
+
+  def item_params
+    params.require(:item).permit(
+      :product,
+      :introduce,
+      :price,
+      :category_id,
+      :condition_id,
+      :shipping_cost_id,
+      :prefecture_id,
+      :shipping_date_id,
+      :image
+    ).merge(user_id: current_user.id)
+  end
 
 end
---
+++ app/models/item.rb
@@ -0,0 +1,41 @@
+class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shipping_cost
+  belongs_to :prefecture
+  belongs_to :shipping_date
+  belongs_to :user
+
+  has_one_attached :image
+
+  # 商品画像が必須
+  validates :image, presence: { message: 'を添付してください' }
+
+  # 商品名・説明・価格が必須
+  validates :product, presence: true
+  validates :introduce, presence: true
+  validates :price, presence: true
+
+  # ActiveHashで「---（id: 1）」以外を選択必須とする
+  with_options numericality: { other_than: 1, message: "can't be blank" } do
+    validates :category_id
+    validates :condition_id
+    validates :shipping_cost_id
+    validates :prefecture_id
+    validates :shipping_date_id
+  end
+
+  # 価格のバリデーション（範囲・数値）
+  validates :price, numericality: {
+    only_integer: true,
+    greater_than_or_equal_to: 300,
+    less_than_or_equal_to: 9_999_999,
+    message: 'は¥300〜¥9,999,999の範囲で入力してください'
+  }
+
+  # 半角数値チェック（整数のみ）
+  validates_format_of :price, with: /\A[0-9]+\z/, message: 'は半角数字で入力してください'
+end
+
